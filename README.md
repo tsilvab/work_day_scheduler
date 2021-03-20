0ce82b76d55e71b40c2810b13817755edbeb2a53
Thefany Silva Bel <thefanyja@gmail.com>
2021-03-19 22:33:38 -0400

    intial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # work_day_scheduler
+##A simple planner for an employee with a busy schedule.
+Row in the time block change in color when time has passed, present time and available time.
+The user can type text in the time blocks and save them into local storage.
+This application runs in the browser and features dynamically updated HTML and CSS powered by jQuery and moment.js.
+jQuery and moment.js links attracted.
+Bootstrap components and google fonts used.
---
+++ script.js
@@ -0,0 +1,59 @@
+let today = function () {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+};
+$(document).ready(function () {
+    today();
+    setInterval(today, 1000);
+});
+
+
+$(".saveBtn").on("click", function () {
+    let text = $(This).siblings("textarea").val();
+    let time = $(this).parent().attr("id");
+
+    // let workDay=[]
+    $("#hour9.text").val(localStorage.getItem("hour9"));
+    $("#hour10.text").val(localStorage.getItem("hour10"));
+    $("#hour11.text").val(localStorage.getItem("hour11"));
+    $("#hour12.text").val(localStorage.getItem("hour12"));
+    $("#hour1.text").val(localStorage.getItem("hour1"));
+    $("#hour2.text").val(localStorage.getItem("hour2"));
+    $("#hour3.text").val(localStorage.getItem("hour2"));
+    $("#hour4.text").val(localStorage.getItem("hour4"));
+    $("#hour5.text").val(localStorage.getItem("hour5"));
+    ;
+})
+
+// const array = localStorage.getItem("workDay");
+// const planner = JSON.parse(array);
+
+// workDay[time].event = text;
+// localStorage.setItem("workDay", JSON.stringify(workDay));
+
+
+function timeLapse() {
+    let currentTime = moment().hour();
+
+    $(".timeBlock").forEach(function () {
+        let timeBlock = parseInt($(this).attr("id").split("hour")[1]);
+
+        if (timeBlock < currentTime) {
+            $(this).addClass("past");
+
+        }
+        else if (timeBlock === currentTime) {
+            $(this).addClass("present");
+
+        }
+        else {
+            $(this).addClass("future");
+        }
+
+    })
+
+    timeLapse();
+}
+
+
+
+
